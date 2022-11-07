ffd1227c411733864dc70993cf8e739a5626edd0
Samiul Islam Shibly <samiul.shibli01@gmail.com>
2022-11-08 00:05:40 +0600

    fixed bug

--- controller/usersController.js
+++ controller/usersController.js
@@ -57,27 +57,29 @@ async function addUser(req, res, next) {
 async function removeUser(req, res, next) {
     try {
         const user = await User.findByIdAndDelete({
-            _id: req.params.id
-        })
-        
-        // remove avatar
+            _id: req.params.id,
+        });
+
+        // remove user avatar if any
         if (user.avatar) {
-            unlink(path.join(__dirname, `../public/uploads/avatars/${user.avatar}`),
-                err => {
-                    if (err) {
-                        console.log(err);
-                    }
+            unlink(
+                path.join(__dirname, `/../public/uploads/avatars/${user.avatar}`),
+                (err) => {
+                    if (err) console.log(err);
                 }
-            )
+            );
         }
-    } catch (error) {
+        res.status(200).json({
+            message: "User was removed successfully!",
+        });
+    } catch (err) {
         res.status(500).json({
             errors: {
                 common: {
-                    msg: "Couldn't delete user!"
-                }
-            }
-        })
+                    msg: "Could not delete the user!",
+                },
+            },
+        });
     }
 }
 
