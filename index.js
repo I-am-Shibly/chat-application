123279e1b57802283a63fbbaee22af05e128bdb1
Samiul Islam Shibly <samiul.shibli01@gmail.com>
2023-02-07 16:02:01 +0600

    source code push

--- index.js
+++ index.js
@@ -1,49 +1,64 @@
-// External imports
-const express = require('express')
-const dotenv = require('dotenv')
-const path = require('path')
-const mongoose = require('mongoose')
-const { urlencoded } = require('express')
-const cookieParser = require('cookie-parser')
+// external imports
+const express = require("express");
+const http = require("http");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const moment = require("moment");
 
 // internal imports
-const { notFoundHandler, errorHandler } = require('./middlewares/common/errorHandler')
-const loginRouter = require('./router/loginRouter')
-const usersRouter = require('./router/usersRouter')
-const inboxRouter = require('./router/inboxRouter')
+const loginRouter = require("./router/loginRouter");
+const usersRouter = require("./router/usersRouter");
+const inboxRouter = require("./router/inboxRouter");
 
-const app = express()
-dotenv.config()
+// internal imports
+const {
+    notFoundHandler,
+    errorHandler,
+} = require("./middlewares/common/errorHandler");
+
+const app = express();
+const server = http.createServer(app);
+dotenv.config();
+
+// socket creation
+const io = require("socket.io")(server);
+global.io = io;
+
+// set comment as app locals
+app.locals.moment = moment;
 
 // database connection
-mongoose.connect(process.env.MONGO_CONNECTION_STRING)
-    .then(() => console.log('Database Connection Successful!'))
-    .catch(err => console.log(err))
+mongoose
+    .connect(process.env.MONGO_CONNECTION_STRING)
+    .then(() => console.log("database connection successful!"))
+    .catch((err) => console.log(err));
 
 // request parsers
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // set view engine
-app.set("view engine", 'ejs')
+app.set("view engine", "ejs");
 
 // set static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-// parse cookies 
-app.use(cookieParser(process.env.COOKIE_SECRET))
+// parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
-// route handling 
-app.use('/', loginRouter)
-app.use('/users', usersRouter)
-app.use('/inbox', inboxRouter)
+// routing setup
+app.use("/", loginRouter);
+app.use("/users", usersRouter);
+app.use("/inbox", inboxRouter);
 
-// 404 error handling
-app.use(notFoundHandler)
+// 404 not found handler
+app.use(notFoundHandler);
 
 // common error handler
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
     console.log(`app listening to port ${process.env.PORT}`);
-})
+});
